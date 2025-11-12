095e4444eb3d9f3c28886a5108b726df8bcb3cf8
CYRTRUS <totae9949@gmail.com>
2025-11-12 20:21:37 +0700

    Refactor the polygon art generator into an object-oriented design with an interactive menu system.

--- polygon_art.py
+++ polygon_art.py
@@ -2,54 +2,118 @@ import turtle
 import random
 
 
-def draw_polygon(num_sides, size, orientation, location, color, border_size):
-    turtle.penup()
-    turtle.goto(location[0], location[1])
-    turtle.setheading(orientation)
-    turtle.color(color)
-    turtle.pensize(border_size)
-    turtle.pendown()
-    for _ in range(num_sides):
-        turtle.forward(size)
-        turtle.left(360/num_sides)
-    turtle.penup()
+class ShapeGenerator:
+    def __init__(self):
+        self.screen = turtle.Screen()
+        self.artist = turtle.Turtle()
+        self.setup_screen()
+
+    def setup_screen(self):
+        self.screen.bgcolor("black")
+        self.screen.colormode(255)
+        self.artist.speed(0)
+        self.artist.hideturtle()
+        self.screen.tracer(0)
+
+    def clear_board(self):
+        self.artist.clear()
+        self.artist.penup()
+        self.artist.home()
+        self.artist.pendown()
+        self.screen.update()
+
+    def get_new_color(self):
+        red = random.randint(0, 255)
+        green = random.randint(0, 255)
+        blue = random.randint(0, 255)
+        return (red, green, blue)
+
+    def reposition(self, reduction_ratio, location, size):
+        self.artist.penup()
+        self.artist.forward(size*(1-reduction_ratio)/2)
+        self.artist.left(90)
+        self.artist.forward(size*(1-reduction_ratio)/2)
+        self.artist.right(90)
+        location[0] = self.artist.pos()[0]
+        location[1] = self.artist.pos()[1]
+
+    def draw_base_shape(self, size, orientation, location, color, border_size, num_side):
+        self.artist.penup()
+        self.artist.goto(location[0], location[1])
+        self.artist.setheading(orientation)
+        self.artist.color(color)
+        self.artist.pensize(border_size)
+        self.artist.pendown()
+        for _ in range(num_side):
+            self.artist.forward(size)
+            self.artist.left(360 / num_side)
+        self.artist.penup()
+
+    def draw_shape(self, num_shape, num_side, num_layer):
+        for _ in range(num_shape):
+            # Generate new random values for each shape
+            reduction_ratio = 0.6
+            size = random.randint(50, 150)
+            orientation = random.randint(0, 360)
+            location = [random.randint(-300, 300),
+                        random.randint(-200, 200)]
+            color = self.get_new_color()
+            border_size = random.randint(1, 6)
+
+            # Loop drawing in smaller version
+            for _ in range(num_layer):
+                self.draw_base_shape(
+                    size, orientation, location, color, border_size, num_side)
+
+                self.reposition(reduction_ratio, location, size)
+                size *= reduction_ratio
+
+    def run(self):
+        while True:
+            choice = input(
+                "Which art do you want to generate? Enter a number between 1 to 9 inclusive: ")
+
+            self.clear_board()
+
+            if choice == "1":
+                self.draw_shape(30, 3, 1)  # 30 triangles, 1 layer each
+
+            elif choice == "2":
+                self.draw_shape(30, 4, 1)  # 30 squares, 1 layer each
+
+            elif choice == "3":
+                self.draw_shape(30, 5, 1)  # 30 pentagons, 1 layer each
+
+            elif choice == "4":
+                # Mix: 10 triangles, 10 squares, 10 pentagons, 1 layer each
+                self.draw_shape(10, 3, 1)
+                self.draw_shape(10, 4, 1)
+                self.draw_shape(10, 5, 1)
+
+            elif choice == "5":
+                self.draw_shape(30, 3, 3)  # 30 triangles, 3 layers each
+
+            elif choice == "6":
+                self.draw_shape(30, 4, 3)  # 30 squares, 3 layers each
+
+            elif choice == "7":
+                self.draw_shape(30, 5, 3)  # 30 pentagons, 3 layers each
+
+            elif choice == "8":
+                # Mix with layers: 10 of each shape, 3 layers each
+                self.draw_shape(10, 3, 3)
+                self.draw_shape(10, 4, 3)
+                self.draw_shape(10, 5, 3)
+
+            elif choice == "9":
+                # All mix: 2 of each shape with 1, 2, and 3 layers
+                for side in range(3, 6):  # triangle, square, pentagon
+                    for layer in range(1, 4):
+                        self.draw_shape(2, side, layer)
+
+            else:
+                print("Invalid choice.")
 
 
-def get_new_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-
-turtle.speed(0)
-turtle.bgcolor('black')
-turtle.tracer(0)
-turtle.colormode(255)
-
-# draw a polygon at a random location, orientation, color, and border line thickness
-num_sides = random.randint(3, 5)  # triangle, square, or pentagon
-size = random.randint(50, 150)
-orientation = random.randint(0, 90)
-location = [random.randint(-300, 300), random.randint(-200, 200)]
-color = get_new_color()
-border_size = random.randint(1, 10)
-draw_polygon(num_sides, size, orientation, location, color, border_size)
-
-# specify a reduction ratio to draw a smaller polygon inside the one above
-reduction_ratio = 0.618
-
-# reposition the turtle and get a new location
-turtle.penup()
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.left(90)
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.right(90)
-location[0] = turtle.pos()[0]
-location[1] = turtle.pos()[1]
-
-# adjust the size according to the reduction ratio
-size *= reduction_ratio
-
-# draw the second polygon embedded inside the original
-draw_polygon(num_sides, size, orientation, location, color, border_size)
-
-# hold the window; close it by clicking the window close 'x' mark
-turtle.done()
+art_gen = ShapeGenerator()
+art_gen.run()
