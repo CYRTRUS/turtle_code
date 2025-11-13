7db3ffd48fd6c33c607e86529f6d3d150a44322b
CYRTRUS <totae9949@gmail.com>
2025-11-13 21:16:26 +0700

    Change number of generated shapes to be random between 20 and 30 by default

--- polygon_art.py
+++ polygon_art.py
@@ -76,41 +76,43 @@ class ShapeGenerator:
 
             self.clear_board()
 
+            num_shape = random.randint(20,30)  # num shape range: 20-30 shapes
+
             if choice == "1":
-                self.draw_shape(30, 3, 1)  # 30 triangles, 1 layer each
+                self.draw_shape(num_shape, 3, 1)  # triangles, 1 layer each
 
             elif choice == "2":
-                self.draw_shape(30, 4, 1)  # 30 squares, 1 layer each
+                self.draw_shape(num_shape, 4, 1)  # squares, 1 layer each
 
             elif choice == "3":
-                self.draw_shape(30, 5, 1)  # 30 pentagons, 1 layer each
+                self.draw_shape(num_shape, 5, 1)  # pentagons, 1 layer each
 
             elif choice == "4":
-                # Mix: 10 triangles, 10 squares, 10 pentagons, 1 layer each
-                self.draw_shape(10, 3, 1)
-                self.draw_shape(10, 4, 1)
-                self.draw_shape(10, 5, 1)
+                # Mix: triangles, squares, pentagons, 1 layer each
+                self.draw_shape(round(num_shape / 3), 3, 1)
+                self.draw_shape(round(num_shape / 3), 4, 1)
+                self.draw_shape(round(num_shape / 3), 5, 1)
 
             elif choice == "5":
-                self.draw_shape(30, 3, 3)  # 30 triangles, 3 layers each
+                self.draw_shape(num_shape, 3, 3)  # triangles, 3 layers each
 
             elif choice == "6":
-                self.draw_shape(30, 4, 3)  # 30 squares, 3 layers each
+                self.draw_shape(num_shape, 4, 3)  # squares, 3 layers each
 
             elif choice == "7":
-                self.draw_shape(30, 5, 3)  # 30 pentagons, 3 layers each
+                self.draw_shape(num_shape, 5, 3)  # pentagons, 3 layers each
 
             elif choice == "8":
-                # Mix with layers: 10 of each shape, 3 layers each
-                self.draw_shape(10, 3, 3)
-                self.draw_shape(10, 4, 3)
-                self.draw_shape(10, 5, 3)
+                # Mix with layers: all shapes, 3 layers each
+                self.draw_shape(round(num_shape / 3), 3, 3)
+                self.draw_shape(round(num_shape / 3), 4, 3)
+                self.draw_shape(round(num_shape / 3), 5, 3)
 
             elif choice == "9":
                 # All mix: 2 of each shape with 1, 2, and 3 layers
                 for side in range(3, 6):  # triangle, square, pentagon
                     for layer in range(1, 4):
-                        self.draw_shape(2, side, layer)
+                        self.draw_shape(round(num_shape / 10), side, layer)
 
             elif choice.lower() == "q":
                 break
